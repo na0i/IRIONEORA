ef77e0fcab6ab2d70cb507060d4434bee0426669
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-09-10 13:41:36 +0900

    chore: 다음진행을 위한 파일삭제
    Former-commit-id: 2ae90e367b27fd41ee7fcdcf321c24dec29f1afa

--- Sub_PJT_1/조정원/KillCorona/src/lab/ssafy/corona/medical/MedicalAction.java
+++
@@ -1,8 +0,0 @@
-package lab.ssafy.corona.medical;
-
-import lab.ssafy.corona.person.Patient;
-
-public interface MedicalAction {
-    void addPatient(Patient p);
-    void removePatient(Patient p);
-}
