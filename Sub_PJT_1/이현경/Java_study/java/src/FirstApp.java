eaa7cbf5fa6996d160f5c0405bea11c6f8491a93
이현경 <hyun.ohenn@gmail.com>
2021-09-13 11:11:54 +0900

    common: delete SubPJT1
    Former-commit-id: 69dbee4529ce833f1fbfe78ff63cbf7b53a43c0b

--- Sub_PJT_1/이현경/Java_study/java/src/FirstApp.java
+++
@@ -1,6 +0,0 @@
-public class FirstApp {
-    public static void main(String args[]) {
-        System.out.println("첫 번째로 작성한 자바 프로그램");
-        System.out.println("FirstApp 애플리케이션입니다ㄴ");
-    }
-}
--- Sub_PJT_1/조정원/array/src/com/ssafy/sub/Sub.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.sub;
-
-public class Sub {
-    com.ssafy.Phone p = new com.ssafy.Phone();
-}
