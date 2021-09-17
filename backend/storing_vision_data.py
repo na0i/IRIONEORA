16fc2cc3ebadfaac4d5ff3948b6e4f82ac36955a
이현경 <hyun.ohenn@gmail.com>
2021-09-17 23:55:32 +0900

    be: get vision data2
    Former-commit-id: 6f6ceba01e89a7f5de60cfcd1b97f0671aa08d8d

--- backend/storing_vision_data.py
+++ backend/storing_vision_data.py
@@ -1,23 +1,37 @@
 # 유물 사진 URL을 넣어서 vision API에 적용해보자!
 # CSV 파일로 만들 것!
-
 import requests
+from io import BytesIO
 
-IMAGE_URL = 'https://emuseum.go.kr/openapi/img?serviceKey=CGlBMoJxMaPqI9InypnTeJr%2FNYurz%2FWIFV%2FelT3gmeubzSCWLaaZIyC1j9Nb0Gq2TFg8U4akJMRftUTpwwFElit8BnY1rqPOl8n6SHQbmu2%2B7pbw1RAFUvph4fbRXnhKMB9wbTkkNe8%3D&imageId=WVBzbjFFcU5iUWlhU0dxRFlMYUZxL2FKbHJxTlk1VGJ0dGdmK1VwbDNCRnlOdFBSRGNubWJnMGRrTUN4aFY3UWliVjJKM2s5VWN3Q1NpdENEZEdUb2ZqeHdXakVVUENq'
-KEY=
+#IMAGE_URL = 'https://emuseum.go.kr/openapi/img?serviceKey=Qhwj%2B5KHmC8OENHvRm3La%2FxMxbXlXv7HD6Zkm8mpG%2Fg%2BiVN%2BGffbevfZoVDr9vDzsHjkpwtUkmEh1N%2BjnKKKPWf5FfRhBAYcOLum5fp5Wyjs8C%2FCR%2FHztEnDm%2FjtSOw5v%2FBVwBaOcAw%3D&imageId=ZUdWVk93RzZrbGEzZE9RMHBBU0FvL210c2s0ck5Id2swSnBTenhTV1dwM1k1QW9UcXYwYXdIWEJWMkllUjdwY05rMEZIb25CeVdrQ3RSK1BMM1JGN1g3SS9Ja2YrZURF'
+#IMAGE_URL = 'https://t1.daumcdn.net/alvolo/_vision/openapi/r2/images/01.jpg'
+KEY= '3ea7603f345b5f0e692c85f233e79d02'
 
 API_URL = 'https://dapi.kakao.com/v2/vision/face/detect'
 
-headers = {}
-headers['Content-Type'] = 'application/x-www-form-urlencoded'
-headers['Authorization'] = 'KakaoAK ' + KEY
+def face_detect():
+    headers = {}
+    headers['Authorization'] = 'KakaoAK ' + KEY
 
-data = { 'image_url':IMAGE_URL }
+    print(headers)
+    files = {'image': ('test.jpg', open('test.jpg','rb')) }
 
-res = requests.post( API_URL, headers=headers, data=data )
+    res = requests.post( API_URL, headers=headers, files=files )
+    if res.status_code == 200:
+        print('success')
+        print(res.text)
+    else:
+        print(F'Fail {res.status_code}')
+        print(res.text)
 
-if res.status_code == 200:
-    print('success')
-    print(res.text)
-else:
-    print(F'Fail {res.status_code}')
+def get_image(url):
+    res = requests.get(url)
+    if res.status_code == 200:
+        with open('test.jpg', 'wb') as f:
+            f.write(res.content)
+            f.flush()
+            f.close()
+
+#get_image(IMAGE_URL)
+#rint(img)
+face_detect()
---
+++ backend/storing_vision_data2.py
@@ -0,0 +1,11 @@
+# 유물 사진 URL은 바로 Vision API에서 인식이 안됨
+
+import requests
+
+API_URL = 'https://dapi.kakao.com/v2/vision/face/detect'
+KEY= '3ea7603f345b5f0e692c85f233e79d02'
+
+def face_detect():
+    headers = {
+        'Content-Type': 
+    }
