3674b5043cb1c3773277d6545cc099a538fba092
cho.jardin <cho.jardin@gmail.com>
2021-09-18 19:47:09 +0900

    be: storing data > add api key
    Former-commit-id: 4bc24ceec7a06ed3f5f204c4a3a5ea4e2dcf3b42

--- backend/scripts/storing_data.py
+++ backend/scripts/storing_data.py
@@ -15,6 +15,8 @@ def run():
                'LV9sokurL8kYjvdTGXfxBvus+4yck/HDl0a9/mzdvKJh0HwM0Z/W9YIPN3FT1yk0ki/r0sFn3wFfkN7u3qMazw==',
                'SqZskQNLBydKAJrTV5fUn3zRuenH7ELym5KvJWma15ABpxIYBeQK15yeq+cLDfiGBiMv8Pt5VFk1H0Sz4lX3yw==',
                'D/3LM/MOiKLwT/BfzNrt4Uv6ItO4Lzcvl4N/g7GkP6U3buUdSei4pY+gDO/YVovYfFq9tRdvPLY+VqzYDkfR7w==',
+               'javtmpZuM82GShOc+dJyc3k5bo3kZ3dGF/eM1wUyCvvLXsbGG/sQz5gR0jfk2hH5OmBCVBPxBl5NqLdcHYv/Ew==',
+               'aEN8Ldpt09YQ8Cc/V7OWiDn89HI5wrT7V2fhm+4XB8YKqvtBGqFFY9z3PTkplks3Azsz8d1y+yjuy1r79CYKYg==',
                ]
 
     for key in API_KEY:
