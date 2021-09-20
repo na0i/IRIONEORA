aabe5a312b3e56681b87274b219c736b3c4672bd
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-09-21 00:18:34 +0900

    fe: pages
    Former-commit-id: 4e7afedfa44599ef7a4133525cf7cebcc0ebd49d

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class MainArtifact(models.Model):
     identification_number = models.CharField(max_length=100)
     image_uri = models.TextField()
+    desc = models.TextField()
+    museum_name = models.CharField(max_length=200, null=True, blank=True)
+    nationality_name = models.CharField(max_length=200, null=True, blank=True)
+    artifact_name = models.CharField(max_length=200, null=True, blank=True)
 
 
 
