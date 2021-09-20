eb130e3a7a002fc2cfcde0e1644d7a3fcb43a1e8
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-09-21 00:14:00 +0900

    fe: start_setting
    Former-commit-id: cb5412abff13f22a2238ea97869144ab58a3a0d4

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MainArtifact(models.Model):
+    identification_number = models.CharField(max_length=100)
+    image_uri = models.TextField()
+
+
+
+
+
