61db354b450417ba957c38114ec4fe67e40c5cdb
na_0_i <nayoi1oo5@gmail.com>
2021-09-23 23:03:24 +0900

    be: like artifacts
    Former-commit-id: 3a83506fc7046ca170933fd8bed85289c999b27d

--- backend/irioneora/urls.py
+++ backend/irioneora/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
@@ -42,6 +43,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('artifacts/', include('artifacts.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/signup/', include('rest_auth.registration.urls')),
 
