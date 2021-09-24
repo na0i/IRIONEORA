ea2c7b49450357cd647da4a972ad57152b9e96bc
cho.jardin <cho.jardin@gmail.com>
2021-09-25 00:41:53 +0900

    be: kakao login
    Former-commit-id: 1b938ea6182d600c42399c95340373890b83ca1e

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 import requests
 
 # 중복확인
@@ -71,24 +71,42 @@ def kakao_login(request):
         print(profile_res.json())
 
         data = {
-            'code': code,
             'access_token': access_token
         }
-        print(data)
+        accept = requests.post(f'http://localhost:8000/accounts/kakao/user/', data=data)
+
         # 로그인
         print(SocialAccount.objects.all().values())
-        if SocialAccount.objects.all().filter(uid=profile_res.json()['id']):
-            print('already in')
-            accept = requests.post(f'http://localhost:8000/accounts/kakao/user/', data=data)
-            print(accept.json())
+        print(SocialAccount.objects.all().filter(uid=13))
+        social_user = SocialAccount.objects.all().filter(uid=profile_res.json()['id'])
+        print(social_user)
+        # if not social_user:
+        #     print('already in')
+        #     user = get_object_or_404(User, i)
+        #     print(accept.json())
+        #     print(accept.json()['key'])
+        #     return Response(accept.json())
         # 회원가입
-        else:
-            print('need signup')
-            accept = requests.post(f'http://localhost:8000/accounts/kakao/user/', data=data)
-            print(accept)
-            print(accept.json())
-            return
+        # else:
+        #     print('need signup')
+        #     accept = requests.post(f'http://localhost:8000/accounts/kakao/user/', data=data)
+        #     print(accept)
+        #     print(accept.json())
 
+        print(social_user.values())
+        print(social_user[0])
+        user = get_object_or_404(User, id=social_user.values()[0]['user_id'])
+        print('----------')
+        print(user.username)
+        print(user.email)
+        print(profile_res.json())
+        print(profile_res.json()['properties'].get('profile_images', ''))
+        # 정보 업데이트
+        user.nickname = profile_res.json()['properties'].get('nickname', '')
+        user.profile_img = profile_res.json()['properties'].get('profile_image', '')
+        user.email = profile_res.json()['kakao_account']['profile'].get('email', '')
+        user.save()
+        return Response(accept.json())
 
     # 카카오 로그인 인증 코드 요청
     else:
