75fab3f08463cc50f509f20e7af7f406005d5d7a
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-09-27 10:47:31 +0900

    fe:settings
    Former-commit-id: c5673708358b3270d4959c1ecb79398455bb2ad4

---
+++ frontend/src/views/artifacts/SearchPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NavBar/>
+  </div>
+</template>
+
+<script>
+import NavBar from '../../components/common/NavBar.vue'
+  export default {
+    name: "SearchPage",
+    components: {
+      NavBar,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
