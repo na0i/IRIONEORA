c414a1f798fd4f4daf1e586e0ddf0daced20b57a
na_0_i <nayoi1oo5@gmail.com>
2021-09-27 19:15:54 +0900

    fe: artifact-detail axios get
    Former-commit-id: e1cf994b597073cbc6772a56df13345362232753

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import Vuex from "vuex";
 import accounts from "@/store/modules/accounts";
 import artifacts from "@/store/modules/artifacts";
+
 Vue.use(Vuex);
+
 export default new Vuex.Store({
   modules: { accounts, artifacts},
 });
