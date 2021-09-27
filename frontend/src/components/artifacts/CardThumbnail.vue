a62da3485d363fb6c8124aefcbf0bade4b1d5852
cho.jardin <cho.jardin@gmail.com>
2021-09-27 11:57:32 +0900

    fe: profile page
    Former-commit-id: 55d410b2a54f55819c15915f97cdf7252fe21d87

---
+++ frontend/src/components/artifacts/CardThumbnail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="card-thumbnail">
+    <h1>CardThumbnail</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardThumbnail"
+}
+</script>
+
+<style lang="scss">
+  @import "src/assets/style/artifacts/_card-thumbnail.scss";
+</style>
