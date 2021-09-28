365f503caa003014e975b047be3bcbe70f4f541c
이현경 <hyun.ohenn@gmail.com>
2021-09-28 19:14:53 +0900

    be: quick revision of user face url
    Former-commit-id: 94df51513b45928e4ad410b339a06aaa0020c508

--- backend/spark/urls.py
+++ backend/spark/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test',views.test),
-    path('userface', views.user_face),
+    path('test/',views.test),
+    path('userface/', views.user_face),
 ]
