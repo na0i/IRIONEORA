99929f49aa99703ed75207b51aa661570c139c38
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-09-28 22:35:21 +0900

    fe: search
    Former-commit-id: b08f8f666c14dd565c4f2d0693a45548847b6d91

--- frontend/src/components/artifacts/SearchBox.vue
+++ frontend/src/components/artifacts/SearchBox.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
     <input type="text" class="search-box" :placeholder="placeholder" v-model="indexWord" @keyup.enter="searchKeyWord()"/>
+    <hr>
+    <div class="search-wrap-div">
+      <SearchCard 
+        v-for="(item,idx) of search_list[1]" 
+        :key="idx"
+        :artifact="item"
+        />
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import SearchCard from './SearchCard.vue'
   export default {
     name:'SearchBox',
+    components: {
+      SearchCard,
+    },
     props: {
       placeholder: {
         type: String,
@@ -22,6 +34,12 @@ import axios from 'axios'
 
         // index case
         indexWord: '',
+        totalCount: 0,
+        search_list : [],
+        response_dict : {},
+        image_uri : "",
+        id_num : "",
+        name : "",
       }
     },
   methods: {
@@ -29,12 +47,29 @@ import axios from 'axios'
       searchKeyWord: function() {
         console.log(this.indexWord)
         axios({
-        method: 'get',
-        url: `http://www.emuseum.go.kr/openapi/relic/list?serviceKey=${this.serviceKey}&indexWord=${this.indexWord}&numOfRows=1&pageNo=1`,
-        // headers: 
-      })
+          method: 'get',
+          url: `/openapi/relic/list?serviceKey=${this.serviceKey}&indexWord=${this.indexWord}&numOfRows=1&pageNo=1`,
+          // headers: 
+        })
         .then((res) => {
-          console.log(res)
+          this.totalCount = parseInt(res.data.totalCount /100)
+          console.log(this.totalCount)
+        })
+        .then(() => {
+          for (var i = 1; i < this.totalCount+2; i++) {
+            axios({
+              method: 'get',
+              url : `/openapi/relic/list?serviceKey=${this.serviceKey}&indexWord=${this.indexWord}&numOfRows=100&pageNo=${i}`, 
+            })
+            .then((res) => {
+              // console.log(res.data.list)
+              this.search_list.concat(res.data.list)
+              console.log(this.search_list)
+              // concat arrayhelpmethod 
+              //https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+              
+            })
+          }
         })
         .catch((err) => {
           console.log(err)
@@ -48,3 +83,4 @@ import axios from 'axios'
 <style lang="scss" scoped>
   @import "src/assets/style/artifacts/_search-box.scss";
 </style>
+
