9be310666e9468afdc661a9f8f60e16b9f6394b1
cho.jardin <cho.jardin@gmail.com>
2021-09-29 22:28:40 +0900

    fe: finish icon button component
    Former-commit-id: 125dfa27cd56449f9fd9b80b9932bd157dc8dcb4

---
+++ frontend/src/components/artifacts/IconButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>
+      {{value}}&nbsp;
+      <span v-html="icon"></span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IconButton",
+  props: ['value', 'icon']
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "src/assets/style/artifacts/_icon-button";
+</style>
--- frontend/src/views/artifacts/DetailPage.vue
+++ frontend/src/views/artifacts/DetailPage.vue
@@ -91,6 +91,6 @@ export default {
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   @import "src/assets/style/artifacts/detail.scss";
 </style>
