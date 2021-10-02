fbcea4d2846ceb248bd35feeadded0f2cbe9afb4
hyunohenn <hyun.ohenn@gmail.com>
2021-10-02 19:00:08 +0900

    be: add response dummy faces data
    Former-commit-id: 59daab3f457e1157125de2f265ae9d557c710e52

--- backend/spark/views.py
+++ backend/spark/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from artifacts.models import Artifact
 # from django.http import HttpResponse
 
 from rest_framework.decorators import api_view
@@ -8,23 +10,23 @@ from rest_framework.response import Response
 
 # Create your views here.
 
-from pyspark.sql import SparkSession
+# from pyspark.sql import SparkSession
 
-spark = SparkSession\
-        .builder\
-        .master('spark://127.0.0.1:7077')\
-        .appName('Python Spark SQL basic example')\
-        .getOrCreate()
+# spark = SparkSession\
+#         .builder\
+#         .master('spark://127.0.0.1:7077')\
+#         .appName('Python Spark SQL basic example')\
+#         .getOrCreate()
 
-def index(request):
-    sc = spark.sparkContext
+# def index(request):
+#     sc = spark.sparkContext
 
-    # json 파일 읽어들이기
-    test = spark.read.json('/home/path/test.json')
+#     # json 파일 읽어들이기
+#     test = spark.read.json('/home/path/test.json')
 
-    # printSchema()로 json파일의 스키마 형태 볼수 있음
-    test.printSchema()
-    return render(request, 'test.html')
+#     # printSchema()로 json파일의 스키마 형태 볼수 있음
+#     test.printSchema()
+#     return render(request, 'test.html')
 # Create your views here.
 
 
@@ -39,4 +41,19 @@ def test(request):
 def user_face(request):
     print(request.data)
 
-    return HttpResponse(200)
+    dummydata = [
+        {'identification': 'PS0100100102102727900000', 'width': 3000, 'height': 2000, 'x': 0.4754312744140625, 'y': 0.6482667846679687, 'w': 0.05687882486979168, 'h': 0.08531817626953131},
+        {'identification': 'PS0100100100900173100000', 'width': 669, 'height': 515, 'x': 0.43930195122734494, 'y': 0.11844135765890473, 'w': 0.08157868663291756, 'h': 0.10597306038569479},
+        # 같은 그림에 두 얼굴
+        {'identification': 'PS0100100100900260500000', 'width': 1592, 'height': 3000, 'x': 0.6141179913851484, 'y': 0.5116527506510417, 'w': 0.08356299472214579, 'h': 0.044344075520833304},        
+        {'identification': 'PS0100100100900260500000', 'width': 1592, 'height': 3000, 'x': 0.3875239003243758, 'y': 0.6396309000651041, 'w': 0.06894641665358042, 'h': 0.036587605794270894},
+    ]
+
+    for i in range(len(dummydata)):
+        artifact_id = dummydata[i]['identification']
+        artifact = get_object_or_404(Artifact, identification_number=artifact_id)
+        print(artifact.image_uri)
+        dummydata[i]['url'] = artifact.image_uri
+
+    return Response(dummydata)
+    # return HttpResponse(200)
