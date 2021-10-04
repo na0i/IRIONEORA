986af1afb85cf673b234279b10131d52808146de
cho.jardin <cho.jardin@gmail.com>
2021-10-04 23:39:15 +0900

    fe: signup page
    Former-commit-id: 7cb031db4f328a59062dd01d86068d624ae9b901

--- frontend/src/views/accounts/SignInUpPage.vue
+++ frontend/src/views/accounts/SignInUpPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="router-wrap">
-      <router-link to="/login" class="router">로그인</router-link>
-      <router-link to="/signup" class="router">회원가입</router-link>
+      <router-link to="/login" class="router" replace>로그인</router-link>
+      <router-link to="/signup" class="router" replace>회원가입</router-link>
     </div>
 
     <router-view></router-view>
