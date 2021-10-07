23d1ea1a860ac8819257de69294842cf7c37fa87
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-10-08 05:37:39 +0900

    fe: error page
    Former-commit-id: d5b04338e7f7c9839825c6ea1765e73a7f363cf0

---
+++ frontend/src/views/common/ErrorPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+
+    <img src="@/assets/images/left-m.png" alt="main page image" class="left-mountain">
+    <img src="@/assets/images/right-m.png" alt="" class="right-mountain">
+    <img src="@/assets/images/right-mountain2.png" alt="" class="middle-mountain">
+    <div class="description">
+      <div class="description-1">
+        죄송합니다 현재 페이지를 찾을 수 없습니다<br>
+      </div>
+      <div class="description-2">
+        존재하지 않은 주소를 입력하셨거나<br>
+      </div>
+       <div class="description-3">
+        요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다<br>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage',
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../assets/style/common/Error.scss";
+</style>
